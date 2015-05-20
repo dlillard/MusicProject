660358e9c4c4d3d1e27e767b20d2d6d57b732e1b
Imadk0201 <Imadk0201@gmail.com>
2015-05-20 17:39:20 -0500

    Initial Spotify Playback Activity Class... Probably will modify soon

--- app/src/main/java/com/dlillard/musicproject/MainActivity.java
+++ app/src/main/java/com/dlillard/musicproject/MainActivity.java
@@ -1,26 +1,27 @@
 package com.dlillard.musicproject;
 
-import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dlillard.musicproject.controller.Playback.SpotifyPlayer;
 import com.dlillard.musicproject.controller.network.SearchAttributeSetsReceiver;
-import com.dlillard.musicproject.controller.network.SoundCloudModule;
 import com.dlillard.musicproject.controller.network.SpotifyModule;
 import com.dlillard.musicproject.controller.ui.AppDrawerAdapter;
 import com.dlillard.musicproject.model.library.AttributeSet;
-import com.dlillard.musicproject.model.library.Song;
 import com.dlillard.musicproject.util.ApplicationContext;
 import com.dlillard.musicproject.view.SearchFragment;
 
@@ -28,8 +29,13 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity implements SearchAttributeSetsReceiver {
+    private Handler overLayHandler = new Handler();
     private FragmentManager fm;
     private SearchFragment searchFragment;
+    private FrameLayout frameLayout;
+    private ImageButton play, forward, backward;
+    private LinearLayout overLay;
+    private int buttonState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,17 +43,29 @@ public class MainActivity extends ActionBarActivity implements SearchAttributeSe
         super.onCreate(savedInstanceState);
         fm=getFragmentManager();
         setContentView(R.layout.activity_main);
-
+        frameLayout = (FrameLayout) findViewById(R.id.content_frame);
+        play = (ImageButton) findViewById(R.id.play);
+        forward = (ImageButton) findViewById(R.id.skipforward);
+        backward = (ImageButton) findViewById(R.id.skipbackward);
+        overLay = (LinearLayout)findViewById(R.id.staticLayout);
+        buttonState = 0;
         AppDrawerAdapter appDrawerAdapter = new AppDrawerAdapter();
         ListView drawer = (ListView) findViewById(R.id.left_drawer);
         drawer.setAdapter(appDrawerAdapter);
 
         SpotifyModule module = new SpotifyModule();
         module.search(this, AttributeSet.AttributeName.TITLE, "I want it that way");
-
+        //Overlay... Search overLayout
         Intent intent = new Intent(context, SpotifyPlayer.class);
 
         //startActivity(intent);
+        frameLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkOverlayVisibility();
+            }
+        });
+
         /*
         RequestQueue queue = Volley.newRequestQueue(this);
 
@@ -69,6 +87,34 @@ public class MainActivity extends ActionBarActivity implements SearchAttributeSe
             }
         });
         queue.add(stringRequest);*/
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Drawable drawablePlay = getResources().getDrawable(R.drawable.play);
+                Drawable drawablePause = getResources().getDrawable(R.drawable.pause);
+                switch(buttonState){
+                    case 0:
+                        play.setImageDrawable(drawablePause);
+                        buttonState = 1;
+                        break;
+                    case 1:
+                        play.setImageDrawable(drawablePlay);
+                        buttonState = 0;
+                        break;
+                }
+            }
+        });
+    }
+    public void checkOverlayVisibility(){
+        overLay.setVisibility(View.VISIBLE);
+        overLayHandler.postDelayed(new Runnable() {
+            public void run() {
+                if(overLay.isShown()){
+                    overLay.setVisibility(View.INVISIBLE);
+                }
+            }
+        }, 10000);
     }
 
     public void onSearchLoaded(String originalSearch, ArrayList<AttributeSet> searchResults){
--- app/src/main/java/com/dlillard/musicproject/controller/Playback/TempSpotifyPlayer.java
+++ app/src/main/java/com/dlillard/musicproject/controller/Playback/TempSpotifyPlayer.java
@@ -1,6 +1,10 @@
 package com.dlillard.musicproject.controller.Playback;
 
 /**
+ * Created by Imad on 5/18/2015.
+ */
+
+/**
  * Created by Imad on 5/14/2015.
  */
 import android.app.Activity;
@@ -9,7 +13,9 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.dlillard.musicproject.R;
+import com.dlillard.musicproject.controller.network.SpotifyModule;
 import com.dlillard.musicproject.model.library.Song;
+import com.dlillard.musicproject.util.ApplicationContext;
 import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
@@ -21,9 +27,15 @@ import com.spotify.sdk.android.player.PlayerNotificationCallback;
 import com.spotify.sdk.android.player.PlayerState;
 
 import java.util.ArrayList;
+import java.util.Queue;
 
-public class SpotifyPlayer extends Activity implements
+public class TempSpotifyPlayer implements
         PlayerNotificationCallback, ConnectionStateCallback, Playback{
+    private ArrayList<Song> queue;
+    public TempSpotifyPlayer(ArrayList<Song> songs){
+        queue.addAll(songs);
+
+    }
 
     // TODO: Replace with your client ID
     private static final String CLIENT_ID = "e8d0c93320fe4ac1a42d3f328496b00c";
@@ -31,36 +43,28 @@ public class SpotifyPlayer extends Activity implements
     private static final String REDIRECT_URI = "myniftyapp://callback";
 
     // Request code that will be passed together with authentication result to the onAuthenticationResult callback
+    // Can be any integer
     private static final int REQUEST_CODE = 1337;
-    private ArrayList<Song> queue;
+
     private Player mPlayer;
     //AttributeSet (songs are a list of attributesets and each attribute set is information recieved from one service (Links)
     //Enum within AttributeSet
 
-    public SpotifyPlayer(ArrayList<Song> songs){
-        queue.addAll(songs);
+
+        /**onCreate Method
+         */
+        public void Authentication() {
+            AuthenticationRequest.Builder builder =
+                    new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+
+            builder.setScopes(new String[]{"user-read-private", "streaming"});
+            AuthenticationRequest request = builder.build();
+
+            //AuthenticationClient.openLoginActivity(ApplicationContext.app, REQUEST_CODE, request);
+        }
 
 
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        AuthenticationRequest.Builder builder =
-                new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
-
-        builder.setScopes(new String[]{"user-read-private", "streaming"});
-        AuthenticationRequest request = builder.build();
-
-        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        // Check if result comes from the correct activity
+    /**protected void onActivityResult
         if (requestCode == REQUEST_CODE) {
             System.out.println("========================================================TEST=========================================================");
             AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
@@ -69,8 +73,8 @@ public class SpotifyPlayer extends Activity implements
                 mPlayer = Spotify.getPlayer(playerConfig, this, new Player.InitializationObserver() {
                     @Override
                     public void onInitialized(Player player) {
-                        mPlayer.addConnectionStateCallback(SpotifyPlayer.this);
-                        mPlayer.addPlayerNotificationCallback(SpotifyPlayer.this);
+                        mPlayer.addConnectionStateCallback(TempSpotifyPlayer.this);
+                        mPlayer.addPlayerNotificationCallback(TempSpotifyPlayer.this);
                         mPlayer.play("spotify:track:3ISTk34XFWxJJnUs5youLa");
 
                     }
@@ -82,7 +86,7 @@ public class SpotifyPlayer extends Activity implements
                 });
             }
         }
-    }
+     */
 
     @Override
     public void onLoggedIn() {
@@ -119,6 +123,7 @@ public class SpotifyPlayer extends Activity implements
         }
     }
     public void play(){
+
     }
 
     public void pause(){
@@ -133,7 +138,7 @@ public class SpotifyPlayer extends Activity implements
         mPlayer.skipToPrevious();
     }
     public ArrayList<Song> getQueue(){
-       return null;
+        return null;
     }
     public Song getCurrentSong(){
         return null;
@@ -149,11 +154,11 @@ public class SpotifyPlayer extends Activity implements
                 break;
         }
     }
-
+/*
     @Override
     protected void onDestroy() {
         // VERY IMPORTANT! This must always be called or else you will leak resources
         Spotify.destroyPlayer(this);
-        super.onDestroy();
-    }
+        //super.onDestroy();
+    }*/
 }
